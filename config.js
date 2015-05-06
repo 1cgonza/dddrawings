39b2dded8ee36b48aed936a4ff519ceb46a999de
Juan Camilo Gonzalez <info@juancgonzalez.com>
2015-05-06 21:17:03 +0800

    remove metadata file, using config instead

---
+++ config.js
@@ -0,0 +1,50 @@
+var environments = {
+  dev: {
+    baseUrl: 'http://localhost:3000/',
+    isDev: true
+  },
+  prod: {
+    baseUrl: 'http://1cgonza.github.io/ddd/',
+    isDev: false
+  }
+};
+
+var defaults = {
+  siteTitle: 'Data Driven Drawings',
+  siteSubtitle: 'An Approach to Autobiographical Animation',
+  author: 'Juan Camilo González',
+  email: 'info@juancgonzalez.com',
+  siteDescription: 'PhD dissertation.',
+  twitter: '@1cgonza',
+  github: '1cgonza',
+  googlePlus: 'https://plus.google.com/+JuanCamiloGonz%C3%A1lezJ/posts',
+  defaultImage: 'https://farm9.staticflickr.com/8592/16218761572_6100bf03d9_o.jpg',
+  defaultThumb: 'https://farm9.staticflickr.com/8592/16218761572_016c2e0f3c.jpg',
+  defaultNoImg: 'https://farm3.staticflickr.com/2950/15297536047_e9da6fd148.jpg',
+  extLibraries: {
+    jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+    jqueryui: '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js',
+    d3: 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js'
+  }
+}
+
+function attachEnvVariables (options) {
+  for (var option in options) {
+    defaults[option] = options[option];
+  }
+  return defaults;
+}
+
+var defineEnvironment = function (args) {
+  var env = attachEnvVariables(environments.dev);
+
+  args.forEach(function (val) {
+    if (val === '--prod' || val === '-p') {
+      env = attachEnvVariables(environments.prod);
+    }
+  });
+
+  return env;
+};
+
+module.exports = defineEnvironment;

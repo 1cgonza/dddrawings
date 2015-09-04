a499d6169ae38a6810e792e79005e3b72fe286d7
Juan Camilo Gonzalez <info@juancgonzalez.com>
2015-09-04 15:32:36 +0800

    create a safe clean module with callback

---
+++ clean.js
@@ -0,0 +1,14 @@
+var chalk = require('chalk');
+var del   = require('del');
+
+function safelyClean (cb) {
+  console.log( chalk.cyan('......................| START cleaning |......................') );
+
+  del(['build/**', '!build', '!build/CNAME', '!build/data/**', '!build/videos/**']).then(function (paths) {
+    console.log( chalk.gray( 'Deleted files/folders:\n', paths.join('\n') ) );
+    console.log( chalk.cyan('......................| FINISHED cleaning |......................') );
+    if (cb) cb();
+  });
+}
+
+module.exports = safelyClean;

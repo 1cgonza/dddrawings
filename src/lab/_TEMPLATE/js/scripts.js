f4db1aa4bef6d4cdc37eba80f865f042c1dbfe4b
Juan Camilo Gonzalez <info@juancgonzalez.com>
2015-10-02 12:23:38 +0800

    update template

--- src/lab/_TEMPLATE/js/scripts.js
+++ src/lab/_TEMPLATE/js/scripts.js
@@ -1,16 +1,17 @@
 (function () {
   'use strict';
-  var loaded    = false;
   var container = document.getElementById('ddd-container');
   var loading   = document.getElementById('ddd-loading');
+  var stageW = window.innerWidth;
+  var stageH = window.innerHeight;
+  var centerX = stageW / 2 | 0;
+  var centerY = stageH / 2 | 0;
 
   /*----------  CREATE CANVAS  ----------*/
   var canvas = document.createElement('canvas');
   var ctx    = canvas.getContext('2d');
-  canvas.width  = window.innerWidth;
-  canvas.height = window.innerHeight;
-  var centerX = canvas.width / 2  | 0;
-  var centerY = canvas.height / 2 | 0;
+  canvas.width  = stageW;
+  canvas.height = stageH;
   container.appendChild(canvas);
 
   function init () {

40f499fd15710aff0df1f6f913cb4e202645b2c7
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-03-04 17:00:11 +0800

    clean styles

--- clean.js
+++ clean.js
@@ -1,8 +1,8 @@
 var chalk = require('chalk');
 var del   = require('del');
 
-function safelyClean (cb) {
-  console.log( chalk.cyan('......................| START cleaning |......................') );
+function safelyClean(cb) {
+  console.log(chalk.cyan('......................| START cleaning |......................'));
 
   del([
     'build/**',
@@ -11,10 +11,12 @@ function safelyClean (cb) {
     '!build/data/**',
     '!build/videos/**',
     '!build/Readme.md'
-  ]).then(function (paths) {
-    console.log( chalk.gray( 'Deleted files/folders:\n', paths.join('\n') ) );
-    console.log( chalk.cyan('......................| FINISHED cleaning |......................') );
-    if (cb) cb();
+  ]).then(function(paths) {
+    console.log(chalk.gray('Deleted files/folders:\n', paths.join('\n')));
+    console.log(chalk.cyan('......................| FINISHED cleaning |......................'));
+    if (cb) {
+      cb();
+    }
   });
 }
 

d4980aa7c2d6d8d6896ea730f9ae5e1750c5fab2
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-03-18 09:26:15 +0800

    fix elements multiplying

--- src/js/ddd/http/req.js
+++ src/js/ddd/http/req.js
@@ -10,28 +10,40 @@ DataRequest.prototype.json = function(url, callback, ret, container, msgLoading,
   this.getD(url, callback, ret, 'application/json', container, msgLoading, loading);
 };
 
-DataRequest.prototype.getD = function(url, callback, ret, type, container, msgLoading, loading) {
+DataRequest.prototype.getD = function(url, callback, ret, type, container, msgLoading, loading, resType) {
   var progress;
   var msg;
 
+  msgLoading = msgLoading || '';
+  container  = container || document.body;
+
   var hasLoading = false;
   var progressBar = false;
+
   if (loading) {
     hasLoading = true;
     loading = loading;
   } else {
-    loading = document.createElement('div');
-  }
+    loading = container.querySelector('.loading');
 
+    if (!loading) {
+      loading = document.createElement('div');
+      container.appendChild(loading);
+    }
+  }
   loading.className = 'loading';
-  msgLoading = msgLoading || '';
-  container  = container || document.body;
 
   this.oReq.onloadstart = displayProgress;
   this.oReq.onprogress = updateProgress.bind(this);
   this.oReq.onloadend = hideProgress;
   this.oReq.onload = function() {
-    var data = this.oReq.responseText;
+    var data;
+    if (resType === 'arraybuffer') {
+      data = this.oReq.response;
+    } else {
+      data = this.oReq.responseText;
+    }
+
     if (type === 'application/json') {
       data = JSON.parse(data);
     }
@@ -51,27 +63,33 @@ DataRequest.prototype.getD = function(url, callback, ret, type, container, msgLo
     this.oReq.overrideMimeType(type);
   }
 
+  if (resType) {
+    this.oReq.responseType = resType;
+  }
+
   this.oReq.send(null);
 
   function updateProgress(event) {
     if (event.lengthComputable) {
       if (!progressBar) {
-        progress              = document.createElement('progress');
-        progress.className    = 'progress';
-        progress.style.zIndex = 0;
-        progress.value        = 0;
-        progress.max          = 100;
-
-        loading.insertBefore(progress, msg);
-        progressBar = true;
+        progress = container.querySelector('.progress');
+        if (!progress) {
+          progress              = document.createElement('progress');
+          progress.className    = 'progress';
+          progress.style.zIndex = 0;
+          progress.max          = 100;
+          progress.value        = 0;
+          loading.insertBefore(progress, msg);
+          progressBar = true;
+        }
       }
       var value = event.loaded / event.total * 100;
       progress.value = value;
       msg.innerText = Math.floor(value) + '%' + '\n' + msgLoading;
     } else {
-      progress = document.createElement('div');
-      progress.className = 'no-progress';
-      loading.insertBefore(progress, msg);
+      // progress = document.createElement('div');
+      // progress.className = 'no-progress';
+      // loading.insertBefore(progress, msg);
       this.oReq.onprogress = null;
     }
   }
@@ -84,13 +102,16 @@ DataRequest.prototype.getD = function(url, callback, ret, type, container, msgLo
   }
 
   function displayProgress() {
+    loading.style.opacity = 1;
     loading.style.zIndex = 9;
-    msg = document.createElement('p');
-    msg.className = 'loading-msg';
-    msg.innerText = msgLoading;
 
-    loading.appendChild(msg);
-    container.appendChild(loading);
+    msg = container.querySelector('.loading-msg');
+    if (!msg) {
+      msg = document.createElement('p');
+      msg.className = 'loading-msg';
+      msg.innerText = msgLoading;
+      loading.appendChild(msg);
+    };
   }
 };
 
@@ -103,8 +124,13 @@ function image(url, callback, ret, container, msgLoading, loading) {
   var req = new DataRequest().getD(url, callback, ret, 'text/plain; charset=x-user-defined', container, msgLoading);
 }
 
+function audio(url, callback, ret, container, msgLoading, loading) {
+  var req = new DataRequest().getD(url, callback, ret, null, container, msgLoading, null, 'arraybuffer');
+}
+
 module.exports = {
   DataRequest: DataRequest,
   json: json,
-  image: image
+  image: image,
+  audio: audio
 };

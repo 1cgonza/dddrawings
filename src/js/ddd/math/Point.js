d72e710808c2cc5da2a8a398a0a7ee457b7c4ecb
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-04-14 14:07:18 +0800

    init flock 2d3d

--- src/js/ddd/math/Point.js
+++ src/js/ddd/math/Point.js
@@ -19,5 +19,5 @@ Point.prototype.equals = function(p) {
 
 Point.prototype.set = function(x, y) {
   this.x = x || 0;
-  this.y = y || ((y !== 0) ? this.x : 0) ;
+  this.y = y || y !== 0 ? this.x : 0;
 };
--- src/js/ddd/math/Vector.js
+++ src/js/ddd/math/Vector.js
@@ -1,14 +1,14 @@
+// From Three.js Vector3
+// https://github.com/mrdoob/three.js/blob/cf584a60bdfd24c42eaa81d484533364742bda44/src/math/Vector3.js
+// All credits to them
+
 var Vector = function(x, y, z) {
-  this.set(x || 0, y || 0, z || 0);
+  this.x = x || 0;
+  this.y = y || 0;
+  this.z = z || 0;
 };
 
 module.exports = Vector;
-
-Vector.prototype.add = function(v, w) {
-  this.x += v.x;
-  this.y += v.y;
-  return this;
-};
 
 Vector.prototype.set = function(x, y, z) {
   this.x = x;
@@ -17,43 +17,112 @@ Vector.prototype.set = function(x, y, z) {
   return this;
 };
 
-Vector.prototype.divideScalar = function(scalar) {
-  return this.multiplyScalar(1 / scalar);
+Vector.prototype.setX = function(x) {
+  this.x = x;
+  return this;
 };
 
-Vector.prototype.length = function() {
-  return Math.sqrt(this.x * this.x + this.y * this.y);
+Vector.prototype.setY = function(y) {
+  this.y = y;
+  return this;
 };
 
-Vector.prototype.distanceTo = function(v) {
-  return Math.sqrt(this.distanceToSquared(v));
+Vector.prototype.setZ = function(z) {
+  this.z = z;
+  return this;
+};
+
+Vector.prototype.add = function(v) {
+  this.x += v.x;
+  this.y += v.y;
+  this.z += v.z;
+  return this;
+};
+
+Vector.prototype.addScalar = function(s) {
+  this.x += s;
+  this.y += s;
+  this.z += s;
+  return this;
+};
+
+Vector.prototype.sub = function(v) {
+  this.x -= v.x;
+  this.y -= v.y;
+  this.z -= v.z;
+  return this;
+};
+
+Vector.prototype.subScalar = function(s) {
+  this.x -= s;
+  this.y -= s;
+  this.z -= s;
+  return this;
 };
 
 Vector.prototype.subVectors = function(a, b) {
   this.x = a.x - b.x;
   this.y = a.y - b.y;
   this.z = a.z - b.z;
-  this.w = a.w - b.w;
-
   return this;
 };
 
-Vector.prototype.normalize = function() {
-  return this.divideScalar(this.length());
+Vector.prototype.multiply = function(v) {
+  this.x *= v.x;
+  this.y *= v.y;
+  this.z *= v.z;
+  return this;
 };
 
-Vector.prototype.multiplyScalar = function(scalar) {
-  if (isFinite(scalar)) {
-    this.x *= scalar;
-    this.y *= scalar;
-    this.z *= scalar;
+Vector.prototype.multiplyScalar = function(s) {
+  if (isFinite(s)) {
+    this.x *= s;
+    this.y *= s;
+    this.z *= s;
   } else {
     this.x = 0;
     this.y = 0;
     this.z = 0;
   }
-
   return this;
+};
+
+Vector.prototype.multiplyVectors = function(a, b) {
+  this.x = a.x * b.x;
+  this.y = a.y * b.y;
+  this.z = a.z * b.z;
+  return this;
+};
+
+Vector.prototype.divide = function(v) {
+  this.x /= v.x;
+  this.y /= v.y;
+  this.z /= v.z;
+  return this;
+};
+
+Vector.prototype.divideScalar = function(s) {
+  return this.multiplyScalar(1 / s);
+};
+
+Vector.prototype.lengthSq = function() {
+  return this.x * this.x + this.y * this.y + this.z * this.z;
+};
+
+Vector.prototype.length = function() {
+  return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+};
+
+Vector.prototype.setLength = function(length) {
+  return this.multiplyScalar(length / this.length());
+};
+
+Vector.prototype.distanceTo = function(v) {
+  return Math.sqrt(this.distanceToSquared(v));
+};
+
+Vector.prototype.normalize = function() {
+  return this.divideScalar(this.length());
 };
 
 Vector.prototype.distanceToSquared = function(v) {
@@ -63,18 +132,10 @@ Vector.prototype.distanceToSquared = function(v) {
   return dx * dx + dy * dy + dz * dz;
 };
 
-Vector.prototype.sub = function(v) {
-  this.x -= v.x;
-  this.y -= v.y;
-  this.z -= v.z;
-
-  return this;
-};
-
-Vector.prototype.copy = function(a) {
-  this.x = a.x;
-  this.y = a.y;
-  this.z = a.z;
+Vector.prototype.copy = function(v) {
+  this.x = v.x;
+  this.y = v.y;
+  this.z = v.z;
   return this;
 };
 

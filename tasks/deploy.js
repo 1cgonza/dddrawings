f316a0a4cad5045b7e757a1d17230c46af9e675b
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-12-25 18:10:47 -0500

    fix deploy paths

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,7 +3,7 @@ var gulpsmith = require('gulpsmith');
 var ghPages   = require('gulp-gh-pages');
 
 function deploy() {
-  gulp.src(['../build/**/*', '!../build/videos', '!../build/videos/**'])
+  gulp.src(['./build/**/*', '!./build/videos', '!./build/videos/**'])
   .pipe(ghPages())
   .pipe(
     gulpsmith()

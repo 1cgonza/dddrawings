900779072f5a352879f8233b305d4a3d4550ce72
Juan Camilo González <info@juancgonzalez.com>
2018-05-13 23:45:57 -0500

    remove comment to fix build error

--- tasks/hbs-helpers/analytics.js
+++ tasks/hbs-helpers/analytics.js
@@ -3,14 +3,12 @@ import metadata from '../config';
 
 module.exports = function() {
   let script = '';
-  if (metadata.env !== 'dev') {
-    script = '< !--Global site tag(gtag.js) - Google Analytics-- >' +
-    '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-18482571-2"></script>' +
-    '<script>window.dataLayer = window.dataLayer || [];' +
-    'function gtag(){dataLayer.push(arguments); }' +
-      'gtag("js", new Date());' +
-      'gtag("config", "UA - 18482571 - 2");</script>';
-  }
-  return new SafeString(script);
 
+  if (metadata.env !== 'dev') {
+    script = '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-18482571-2"></script>' +
+      '<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}' +
+      'gtag("js", new Date());gtag("config", "UA-18482571-2");</script>';
+  }
+
+  return new SafeString(script);
 };

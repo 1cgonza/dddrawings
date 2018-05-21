17c6a59a543a04f4ec970b74495255e7c2eee67c
Juan Camilo González <info@juancgonzalez.com>
2018-05-21 08:12:20 -0500

    memory

--- js/lab/vista-al-mar/Memory.js
+++
@@ -1,5 +0,0 @@
-var Memory = function() {
-  
-};
-
-export default Memory;
--- js/lab/vista-al-mar/index.js
+++ js/lab/vista-al-mar/index.js
@@ -3,51 +3,54 @@ import Mask from './Mask';
 import Map from './Map';
 import Audio from './Audio';
 import Menu from './Menu';
+import MemoryPalace from './MemoryPalace';
 
 /*----------  SET STAGE  ----------*/
-var container = document.getElementById('ddd-container');
-var animReq;
-var notations = new Notations(container);
-var mask = new Mask(container);
-var map = new Map(container);
-var audio = new Audio();
+const container = document.getElementById('ddd-container');
+const notations = new Notations(container);
+const mask = new Mask(container);
+const map = new Map(container);
+const audio = new Audio();
+const memory = new MemoryPalace(container);
 
 window.init = function() {
   map.init();
 };
 
 /*----------  GLOBALS  ----------*/
-var pulseData = [];
-var pulseDataLoaded = false;
-var dataLenght = 0;
-var dataI = 0;
-var maxBeat = 563;
-var minBeat = 462;
-var beatRange = maxBeat - minBeat;
-var beatStep = 1 / beatRange;
+let animReq;
+let pulseData = [];
+let pulseDataLoaded = false;
+let dataLenght = 0;
+let dataI = 0;
+let maxBeat = 563;
+let minBeat = 462;
+let beatRange = maxBeat - minBeat;
+let beatStep = 1 / beatRange;
 
-var pulse = DDD.json('/data/pulse/heart.2.json')
-  .then(ret => {
-    for (var i = 0; i < ret.beats.length; i++) {
-      var d = ret.beats[i];
-      if (d.charAt(0) === 'S') {
-        pulseData.push(+d.substr(1));
-      }
+let pulse = DDD.json('/data/pulse/heart.2.json').then(ret => {
+  for (let i = 0; i < ret.beats.length; i++) {
+    let d = ret.beats[i];
+    if (d.charAt(0) === 'S') {
+      pulseData.push(+d.substr(1));
     }
-    pulseDataLoaded = true;
-    dataLenght = pulseData.length;
-    notations.bindData(pulseData);
-    notations.init();
-    heart();
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  }
+  pulseDataLoaded = true;
+  dataLenght = pulseData.length;
+  notations.bindData(pulseData);
+  notations.init();
+  heart();
+  memory.invokeStatic();
+}).catch(err => {
+  console.error(err);
+});
 
 function heart() {
   if (dataI < dataLenght) {
-    var beat = pulseData[dataI];
-    map.layer.style.opacity = (maxBeat - beat - (beatRange / 3)) * beatStep;
+    let beat = pulseData[dataI];
+    let o = (maxBeat - beat) * beatStep;
+    map.layer.style.opacity = o;
+    memory.updateOpacity(o);
     notations.update(dataI);
     dataI++;
   } else {

2cad7b6eb4b4922d95e208fb0ba8b4ab7650e9a2
Juan Camilo González <info@juancgonzalez.com>
2019-10-29 22:42:23 -0500

    fix server and exports

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-import deploy from './tasks/deploy';
-import clean from './tasks/clean';
-import build from './tasks/build';
-import metadata from './tasks/config';
-import ImgManager from './tasks/Images-manager';
+import deploy from './tasks/deploy'
+import clean from './tasks/clean'
+import { build, serve } from './tasks/build'
+import metadata from './tasks/config'
+import ImgManager from './tasks/Images-manager'
 
 function init() {
   if (metadata.env === 'clean') {
-    clean();
+    clean()
   } else if (metadata.env === 'deploy') {
-    clean(buildAndDeploy);
+    clean(buildAndDeploy)
   } else if (metadata.env === 'prod') {
-    build.once();
+    build()
   } else {
     // The default mode will be development
     // This will launch browserSync to work locally
-    build.serve();
+    serve()
   }
 }
 
 function buildAndDeploy() {
-  build.once(deploy);
+  build(deploy)
 }
 
 if (process.argv.indexOf('--img') >= 0) {
   var options = {
     force: true,
     log: 'verbose'
-  };
-  var manager = new ImgManager();
+  }
+  var manager = new ImgManager()
 
-  manager.process(options, init);
+  manager.process(options, init)
 } else {
-  init();
+  init()
 }
--- tasks/build-plugins/server.js
+++ tasks/build-plugins/server.js
@@ -1,42 +1,38 @@
-import chalk from 'chalk';
-import browserSync from 'browser-sync';
-import connectGzip from 'connect-gzip-static';
+import chalk from 'chalk'
+import browserSync from 'browser-sync'
+import connectGzip from 'connect-gzip-static'
 
-const server = browserSync.create();
-const gzip = connectGzip('./build');
-
-module.exports = (buildCallback) => {
+export default buildCallback => {
+  const server = browserSync.create()
+  const gzip = connectGzip('./build')
   const config = {
+    watch: true,
     server: 'build',
-    files: [{
-      match: [
-        '**/*',
-        '!build/**.*',
-        '!src/img/lab/**.*',
-        '!src/img/notations/**.*',
-        '!.git/**.*',
-        '!**/*.DS_Store'
-      ],
-      fn: (event, file) => {
-        if (event === 'change') {
-          buildCallback(server.reload);
-          console.log(chalk.cyan('Updated file: ') + chalk.yellow(file));
+    files: [
+      {
+        match: ['src'],
+        fn: (event, file) => {
+          if (event === 'change') {
+            buildCallback(server.reload)
+            console.log(chalk.cyan('Updated file: ') + chalk.yellow(file))
+          }
+        },
+        options: {
+          ignored: ['build/**.*', 'src/img/lab/**.*', 'src/img/notations/**.*']
         }
       }
-    }],
+    ],
     // logLevel: 'debug',
     notify: false,
     open: false
-  };
+  }
 
-  return () => {
-    server.init(config, (err, bs) => {
-      buildCallback();
+  server.init(config, (err, bs) => {
+    buildCallback()
 
-      // uncomment to test gzip delivery on localhost. Just like this it breaks the autoreload
-      // bs.addMiddleware('*', gzip, {
-      //   override: true
-      // });
-    });
-  };
-};
+    // uncomment to test gzip delivery on localhost. Just like this it breaks the autoreload
+    // bs.addMiddleware('*', gzip, {
+    //   override: true
+    // });
+  })
+}

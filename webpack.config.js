f8c11bd7afebc3d43f507330c0cbe07285ad5078
Juan Camilo González <info@juancgonzalez.com>
2019-10-29 22:40:43 -0500

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const resolve = require('path').resolve;
-const webpack = require('webpack');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const fs = require('fs');
+const resolve = require('path').resolve
+const webpack = require('webpack')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const fs = require('fs')
 
 const entries = (function(base) {
   let ret = {
     site: './js/site.js'
-  };
-
-  function appendEntries(folder) {
-    let folders = fs.readdirSync(base + folder);
-    folders.forEach(function(name) {
-      ret[name] = base + folder + '/' + name + '/index.js';
-    });
   }
 
-  appendEntries('/lab');
-  appendEntries('/notations');
+  function appendEntries(folder) {
+    let folders = fs.readdirSync(base + folder)
+    folders.forEach(function(name) {
+      ret[name] = base + folder + '/' + name + '/index.js'
+    })
+  }
 
-  return ret;
-})('./js');
+  appendEntries('/lab')
+  appendEntries('/notations')
+
+  return ret
+})('./js')
 
 module.exports = {
   entry: entries,
@@ -28,24 +28,16 @@ module.exports = {
     path: resolve(__dirname, 'src', 'js'),
     filename: '[name].[chunkhash:4].js'
   },
-  plugins: [
-    new ManifestPlugin(),
-    new CleanWebpackPlugin(['src/js/*.*', 'build/js/*.*'], {
-      root: resolve(__dirname, 'src', 'js'),
-      exclude: ['ddd.min.js', 'ddd.min.js.map', 'vendor/*.*']
-    })
-  ],
+  plugins: [new ManifestPlugin(), new CleanWebpackPlugin()],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: [/node_modules/],
-        loader: 'babel-loader',
-        options: {
-          presets: ['env'],
-          plugins: ['transform-class-properties']
+        use: {
+          loader: 'babel-loader'
         }
       }
     ]
   }
-};
+}
